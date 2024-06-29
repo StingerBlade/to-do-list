4fd80a3366d7b7f025f3e667c6249b428a32183b
StingerBlade <arras2805200@gmail.com>
2024-06-28 18:10:44 -0600

    boton check perron

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ var total = 0;
 //funcion que agrega la tarea al listado
 btn.onclick = function () {
   // controlamos si el campo esta vacio
-  if(inputTarea.value == ""){
-    
+  if (inputTarea.value == "") {
+
     alert("no puede dejar el campo vacio")
     return;
   }
@@ -24,32 +24,55 @@ btn.onclick = function () {
 
   //agrego el li al listado
   listado.appendChild(li);
-  
-  
- 
+
+
+
 
 
   //incremento la cantidad de tareas
-  total ++;
+  total++;
   cantidad.innerHTML = total;
+
+
+  //agregar check a cada elemento li
+  var btncheck = document.createElement("input")
+  btncheck.className = "uk-checkbox";
+  btncheck.type = "checkbox";
+  li.appendChild(btncheck)
+
+  //cuando se haga el check que se tache la tarea
+  btncheck.onclick = function () {
+    if (btncheck.checked) {
+      li.style.textDecoration = 'line-through';
+    }
+    else {
+      li.style.textDecoration = 'none';
+    }
+  }
+  //cuando se desmarque que vuelva a su estado normal
+  btncheck.after = function () {
+
+  }
 
   //agregamos el boton de eliminar a cada elemnto li
   var btneliminar = document.createElement("span");
   btneliminar.textContent = "X";
+  btneliminar.className = "uk-text-danger";
   // btnEliminar.className = "uk-text-danger uk-margin-small-left"; 
   // POR X RAZON SI PONGO ESE ESTILO NO APARECE EL BOTON
 
   li.appendChild(btneliminar);
 
-//agregamos la funcionalidad que elimina la tarea
-btneliminar.onclick = function(){
-  li.remove();
-  total-- ;
-  cantidad.innerHTML = total;
-}
+  //agregamos la funcionalidad que elimina la tarea
+  btneliminar.onclick = function () {
+    li.remove();
+    total--;
+    cantidad.innerHTML = total;
+  }
 
 
-//limpiamos el campo input 
-inputTarea.value = "";
+
+  //limpiamos el campo input 
+  inputTarea.value = "";
 
 }
